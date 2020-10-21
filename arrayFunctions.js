2effe895f5d49e229a89cd2eac26f8eb22533713
Abdullah Alsharrah <abdullahalsharrah@Abdullahs-Air.hotspot>
2020-10-21 16:18:29 +0300

    solved

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -118,18 +118,20 @@ console.log(secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi"]))
  */
 function youGottaCalmDown(s) {
   // Your code here
-    if (s.endsWith("!")) {
-     let i = 0
-     while (s[i] !== "!"){
-       i ++;
-     }
-     return s.slice(0, i+1)
+    // if (s.endsWith("!")) {
+    //  let i = 0
+    //  while (s[i] !== "!"){
+    //    i ++;
+    //  }
+    //  return s.slice(0, i+1)
 
-    } else {
-      return s
+    // } else {
+    //   return s
+    // }
+    while(s.endsWith("!!")) {
+      s = s.slice(0, -1);
     }
-
-
+    return s;
 }
 console.log(youGottaCalmDown("hello!!!!!"));
 
